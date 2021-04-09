9ff677a79239832b6afb1e67ff4390f3c85331cf
Pazia Ramirez <pazia008@gmail.com>
2021-04-09 11:49:05 -0500

    got my bean form so show

--- js-app/main.js
+++ js-app/main.js
@@ -3,6 +3,8 @@ const url2 = "https://localhost:5001/api/coffee/";
 
 
 const beanTarget = document.querySelector(".bean__container")
+const saveMaBeanButton = document.getElementById('saveMaBeans')
+const beanTargetForm = document.querySelector(".bean__form")
 
 const beanButton = document.querySelector("#run-button");
 beanButton.addEventListener("click", () => {
@@ -22,19 +24,39 @@ const getAllBeanVarieties = () => fetch(url).then(resp => resp.json());
 
 
 
+const saveMaBeans = bean => {
+    return fetch(url,
+        {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(bean)
+        })
+};
 
+saveMaBeanButton.addEventListener("click", e => {
+    let name = document.getElementById("bean--name").value;
+    let region = document.getElementById("bean--region").value;
+    let notes = document.getElementById("bean--notes").value;
+    if (notes === "") {
+        notes = null;
+    }
 
-// const listBeans = (beanVarieties) => {
-//     var beanPods = "";
-//     beanVarieties.forEach(bv => {
-//         beanPods += `
-//         <div class="beans">
-//         <div class ="bean__name">${bv.Name}</div>
-//         <div class ="bean__region">${bv.Region}</div>
-//         <div class ="bean__notes">${bv.Notes}</div>
-//         </div>
-//         `
-//     });
-//     return beanPods
-// }
+    if (name.length > 3 && name.length < 55 && region.length > 3 && region.length && region.length < 255) {
+        const newBeanz = {
+            name,
+            region,
+            notes
+        };
 
+        saveMaBeans(newBeanz);
+        beanTargetForm.clear();
+        e.preventDefault();
+    }
+
+    else {
+        alert("Please fill out region and name with more than three characters. Notes are up to you!");
+    }
+
+})
